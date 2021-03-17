a52720dad4fc246c1e599cd35e1cc7a91bd51c89
HansRobrecht <79218348+HansRobrecht@users.noreply.github.com>
2021-03-17 19:46:28 +0100

    Killplane resets ball location to (0,5,0) upon contact

---
+++ Assets/Scripts/ResetBallLocation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetBallLocation : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("OnCollisionEnter - Resetting ballLocation");
+        Debug.Log(other.gameObject.name);
+        Vector3 startPosition = new Vector3(0, 5, 0);
+        other.gameObject.transform.position = startPosition;
+    }
+}
