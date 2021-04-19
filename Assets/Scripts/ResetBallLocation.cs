2fc0858fe67958ad9c74045cf2613e9920f74e88
lucavanhulle <79218143+lucavanhulle@users.noreply.github.com>
2021-04-19 15:54:08 +0200

    tweaks

--- Assets/Scripts/ResetBallLocation.cs
+++ Assets/Scripts/ResetBallLocation.cs
@@ -8,7 +8,9 @@ public class ResetBallLocation : MonoBehaviour
     {
         Debug.Log("OnCollisionEnter - Resetting ballLocation");
         Debug.Log(other.gameObject.name);
-        Vector3 startPosition = new Vector3(0, 5, 0);
+        Vector3 startPosition = new Vector3(0, 0.5f, 0);
         other.gameObject.transform.position = startPosition;
+        other.rigidbody.velocity = Vector3.zero;
+        other.rigidbody.angularVelocity = Vector3.zero;
     }
 }
