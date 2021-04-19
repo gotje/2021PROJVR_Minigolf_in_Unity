3da6b4621ef36ba378b1db2d13105a3b8da1caa6
HansRobrecht <79218348+HansRobrecht@users.noreply.github.com>
2021-04-19 16:17:25 +0200

    Script add velocity

---
+++ Assets/addVelocityToBall.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addVelocityToBall : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        other.rigidbody.velocity = GetComponent<Rigidbody>().velocity;
+    }
+}
