6b9ad99c8d6662ef8bb8ddaedcc6d46653074cb1
HansRobrecht <79218348+HansRobrecht@users.noreply.github.com>
2021-04-22 22:50:06 +0200

    Change to Ball_Collider
    Automatic generation of teleportLocations

--- Assets/Ball_Collider.cs
+++ Assets/Ball_Collider.cs
@@ -10,15 +10,20 @@ public class Ball_Collider : MonoBehaviour
 
     public void Main()
     {
-        teleportLocations.Add(new Vector3(0, 5, 0));
-        teleportLocations.Add(new Vector3(40, 5f, -15));
+        //teleportLocations.Add(new Vector3(0, 5, 0));
+        //teleportLocations.Add(new Vector3(40, 5f, -15));
+        GameObject[] startPoints = GameObject.FindGameObjectsWithTag("startPoint");
+        foreach(GameObject startpoint in startPoints)
+        {
+            teleportLocations.Add(new Vector3(startpoint.transform.position.x, startpoint.transform.position.y+1, startpoint.transform.position.z));
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
         GameObject vrRig = GameObject.FindGameObjectWithTag("Player");
         Debug.Log("Ball is in hole");
-        Debug.Log((Vector3)teleportLocations[Int32.Parse(this.gameObject.name.Split('_')[1] + 1)]);
+        //Debug.Log((Vector3)teleportLocations[Int32.Parse(this.gameObject.name.Split('_')[1] + 1)]);
         currentHole++;
         //other.gameObject.transform.position = (Vector3)teleportLocations[Int32.Parse(this.gameObject.name.Split('_')[1] + 1)];
         //vrRig.transform.position = (Vector3)teleportLocations[Int32.Parse(this.gameObject.name.Split('_')[1] + 1)];
