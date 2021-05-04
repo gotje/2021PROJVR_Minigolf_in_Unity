89a167f9536bd5ac628ed5a623c11cf703d27b7e
HansRobrecht <79218348+HansRobrecht@users.noreply.github.com>
2021-05-04 21:36:51 +0200

    Playing with baseball mechanics

---
+++ Assets/ShootBall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBall : MonoBehaviour
+{
+    GameObject baseball;
+
+    void Main()
+    {
+        baseball = GameObject.Find("Baseball");
+        baseball.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -5000);
+    }
+
+    
+}
