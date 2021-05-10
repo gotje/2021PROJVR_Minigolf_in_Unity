8ee52162f4f043eb105d002c8a7ff165395070f6
Gauthier Vandenabeele <gauthier.vandenabeele@student.odisee.be>
2021-05-10 21:39:07 +0200

    collision detection edit

---
+++ Assets/collisionSpeed.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class collisionSpeed : MonoBehaviour
+{
+    Vector3 previous;
+    float velocity;
+
+    void Start()
+    {
+        previous = transform.position;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "pir (2)")
+        {
+            velocity = ((transform.position - previous).magnitude) / Time.deltaTime;
+            collision.rigidbody.velocity = previous * 3;
+        }
+    }
+    void Update()
+    {
+        velocity = ((transform.position - previous).magnitude) / Time.deltaTime;
+        previous = transform.position;
+    }
+}
+
+
