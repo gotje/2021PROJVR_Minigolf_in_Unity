c83bf893ba5b40809aa1d03035bfbc316b817f99
HansRobrecht <79218348+HansRobrecht@users.noreply.github.com>
2021-05-18 20:23:41 +0200

    Added distanceboard in baseballscene

---
+++ Assets/UpdateDistance.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateDistance : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        UpdateScreen();
+    }
+
+    private void UpdateScreen()
+    {
+        Vector3 baseballLocation = GameObject.Find("Baseball").transform.position;
+        Vector3 playerLocation = GameObject.FindGameObjectWithTag("Player").transform.position;
+        float distance = Mathf.Sqrt(Mathf.Pow((baseballLocation.x - playerLocation.x), 2) + Mathf.Pow((baseballLocation.z - playerLocation.z),2));
+        this.GetComponentInChildren<Text>().text = "" + (float)Mathf.Round(distance*100)/100;
+    }
+}
