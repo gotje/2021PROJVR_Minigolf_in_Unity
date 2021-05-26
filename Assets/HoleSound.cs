b03835977afc5554da245d04dd614fd52b2c0902
unknown <stan.vandenbroucke@student.odisee.be>
2021-05-26 12:06:34 +0200

    Script cleaned

--- Assets/HoleSound.cs
+++ Assets/HoleSound.cs
@@ -7,18 +7,16 @@ using System.Threading.Tasks;
 
 public class HoleSound : MonoBehaviour
 {
-	public AudioClip BallDrop;
-	AudioSource soundSource;
-	RaycastHit hitInfo;
 
     // Start is called before the first frame update
     void Start()
     {
-    	soundSource = GetComponent<AudioSource>();
-    }
+    	    }
 
     // Update is called once per frame
     void Update()
     {
 
 
+        }
+}
