546e649b74b87a12d826eeb6d95d72317b2a3509
lucavanhulle <luca.vanhulle@student.odisee.be>
2021-05-26 17:01:38 +0200

    collision aanpassingen

--- Assets/collisionSpeed.cs
+++ Assets/collisionSpeed.cs
@@ -29,51 +29,23 @@ public class collisionSpeed : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        frames++;
-        this.gameObject.layer = freedLayer;
-        if (frames > 4)
-        {
             if (collision.gameObject.tag == "golfBall")
             {
-                frames = 0;
-                float delta = (float)DateTime.Now.Millisecond - prevTimeColl;
-                velocity = new Vector3(transform.position.x - previousColl.x, 0, transform.position.z - previousColl.z) / delta;
-                Debug.Log(velocity);
-                this.gameObject.layer = restrictedLayer;
-                collision.rigidbody.velocity = velocity * 5;
-                Debug.Log(collision.rigidbody.velocity);
-                collisionSound();
+                collision.rigidbody.velocity = collision.rigidbody.velocity * 130;
             }
-        }
-        prevTimeColl = (float)DateTime.Now.Millisecond;
-        previousColl = transform.position;
     }
 
     void FixedUpdate()
     {
-        frames++;
-        this.gameObject.layer = freedLayer;
-        if (frames > 4)
+        Ray ray = new Ray(myRigidbody.position, previous);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
         {
-            Ray ray = new Ray(myRigidbody.position, previous);
-            RaycastHit hitInfo;
-            if (Physics.Raycast(ray, out hitInfo))
+            if (hitInfo.collider.tag == "golfBall")
             {
-                if (hitInfo.collider.tag == "golfBall")
-                {
-                    frames = 0;
-                    float delta = (float)DateTime.Now.Millisecond - prevTimeFixedUpdate;
-                    velocity = new Vector3(transform.position.x - previous.x, 0, transform.position.z - previous.z) / delta * 5;
-                    this.gameObject.layer = restrictedLayer;
-                    velocity.x = 0;
-                    GameObject.Find("GolfBall").GetComponent<Rigidbody>().velocity = velocity;
-                    collisionSound();
-                }
+                GameObject.Find("GolfBall").GetComponent<Rigidbody>().velocity = GameObject.Find("GolfBall").GetComponent<Rigidbody>().velocity * 130;
             }
         }
-        previous = transform.position;
-        prevTimeFixedUpdate = (float)DateTime.Now.Millisecond;
-        
     }
     void collisionSound(){
     	//play sound ball collision putter
